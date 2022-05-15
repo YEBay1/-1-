d5ce8205fa6fa4996d0fdc52a1bddbbd0a4f6481
YEBay1 <104377308+YEBay1@users.noreply.github.com>
2022-05-15 20:52:34 +0300

    Create İngilizceKelime.cpp

---
+++ İngilizceKelime.cpp
@@ -0,0 +1,31 @@
+	string kelimeler1[4]={"Glass","Human","Cat","Air",};
+	string ceviriler1[4]={"Cam","insan","Kedi","Hava"};
+	string ceviriler2[4]={"cam","insan","kedi","hava"};
+	string ceviriler3[4]={"CAM","INSAN","KEDİ","HAVA"};
+	string c1;
+
+	
+	for(int i=0;i<4;i++){
+		cout<<i+1<<" - "<<kelimeler1[i]<<endl;
+		cin>>c1;
+		cout<<"Girilen Cevap -> "<<c1<<endl;
+		
+		if (c1==ceviriler1[i]){
+			cout<<"Dogru Cevap --> "<<ceviriler1[i]<<endl;
+			cout<<"Dogru Bildiniz!"<<"\n"<<endl;
+		}
+		else if(c1==ceviriler2[i]){
+			cout<<"Dogru Cevap --> "<<ceviriler1[i]<<endl;
+			cout<<"Dogru Bildiniz!"<<"\n"<<endl;
+		}
+		else if(c1==ceviriler3[i]){
+			cout<<"Dogru Cevap --> "<<ceviriler1[i]<<endl;
+			cout<<"Dogru Bildiniz!"<<"\n"<<endl;
+		}
+		else{
+			cout<<"Yanlis Cevap \n"<<endl;
+			cout<<"Dogru Cevap --> "<<ceviriler1[i]<<"\n"<<endl;;
+		}
+	}
+	
+	return 0;
