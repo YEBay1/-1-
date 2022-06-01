df7f0a1147e749bdf02c95e78c0b846e54684f1e
YEBay1 <104377308+YEBay1@users.noreply.github.com>
2022-06-01 16:46:18 +0300

    Update Soccers.py

--- Soccers.py
+++ Soccers.py
@@ -10,10 +10,10 @@ SA=soccer_data.serie_a()
 RUS=soccer_data.russian_premier()
 ENCHAMP=soccer_data.english_championship()
 
-print("League Codes","English Premier --> EN","LaLiga --> LA","Ligue1 --> LI1","Bundesliga --> BU","SerieA --> SA","Russian Premier --> RUS","English Championship --> ENCHAMP",sep="\n")
+print("League Codes","English Premier --> 1","LaLiga --> 2","Ligue1 --> 3","Bundesliga --> 4","SerieA --> 5","Russian Premier --> 6","English Championship --> 7","\n",sep="\n")
 
-b=input()
-c={"EN":EN,"LA":LA,"LI1":LI1,"BU":BU,"SA":SA,"RUS":RUS,"ENCHAMP":ENCHAMP}
+b=input("Lig Kodunu Giriniz --> ")
+c={"1":EN,"2":LA,"3":LI1,"4":BU,"5":SA,"6":RUS,"7":ENCHAMP}
 a=c[b]
 
 print("    Team                           Matches      Wins     Losses     Goals For    Goals Against     Points")
@@ -25,5 +25,9 @@ for i in range(len(a)):
     else:
         print(125 * "-")
         print("0"+a[i]['pos'],"-",a[i]['team'], (30-len(a[i]['team'])) * " ", a[i]['matches_played'],8*" ",a[i]['wins'],5*" ",a[i]['losses'],8*" ", a[i]['goals_for'],10*" ",a[i]['goals_against'],14*" ",a[i]['points'])
+
+
         
-        
+
+
+
