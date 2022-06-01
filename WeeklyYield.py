0e3909506188ca7ed0640980059657d2d2498009
YEBay1 <104377308+YEBay1@users.noreply.github.com>
2022-06-01 17:05:37 +0300

    Update WeeklyYield.py

--- WeeklyYield.py
+++ WeeklyYield.py
@@ -1,31 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-for i in range(1):
-    try:
-        ptesi=float(input("Pazartesi -> "))
-        sali=float(input("Salı -> "))
-        csamba=float(input("Çarşamba -> "))
-        psembe=float(input("Perşembe -> "))
-        cuma=float(input("Cuma -> "))
-        ctesi=float(input("Cumartesi -> "))
-        pazar=float(input("Pazar -> "))
+def HaftalıkVerimHesaplama():
+    for i in range(1):
+        try:
+            a=float(input("Pazartesi -> "))
+            b=float(input("Salı -> "))
+            c=float(input("Çarşamba -> "))
+            d=float(input("Perşembe -> "))
+            e=float(input("Cuma -> "))
+            f=float(input("Cumartesi -> "))
+            g=float(input("Pazar -> "))
 
-    except ValueError:
-        print("Lütfen Bir Sayı Giriniz")
-
-haftx = np.array([1,2,3,4,5,6,7])
-saaty = np.array([ptesi,sali,csamba,psembe,cuma,ctesi,pazar])
-plt.plot(haftx, saaty)
-plt.show()
-
-günlükort=float(ptesi+sali+csamba+psembe+cuma+ctesi+pazar)/7
-saat=int(günlükort)
-dakika=günlükort-int(günlükort)
-sonuc=(saat*60)+dakika
-print("Bu hafta Günlük Ortalama ",int(sonuc)," dakika çalışıldı")
+        except ValueError:
+            print("Lütfen Bir Sayı Giriniz")
 
 
+    günlükort=float(a+b+c+d+e+f+g)/7
+
+    if günlükort>=24:
+        print("HATALI VERİ GİRMİŞ OLABİLİRSİNİZ.LÜTFEN GİRDİĞİNİZ VERİLERİ KONTROL EDİN VEYA UYGULAMAYI TEKRAR ÇALIŞTIRIN")
+    else:
+        saat = int(günlükort)
+        dakika = günlükort - int(günlükort)
+        dk = (saat * 60) + dakika
+        print("Bu hafta Günlük Ortalama ", int(dk), " dakika çalışıldı","\n")
 
 
-
+    Q=input("Haftalık Verim Grafiğini Görmek için Q'ya tıklayın ve Girin --->")
+    if Q=="Q":
+        haftx = np.array(["PTESİ","SALI","ÇŞAMBA","PŞEMBE","CUMA","CTESİ","PAZAR"])
+        saaty = np.array([a, b, c, d, e, f, g])
+        plt.plot(haftx, saaty)
+        plt.show()
+    else:
+        pass
