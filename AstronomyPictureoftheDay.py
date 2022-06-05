a06c3aaf8dcd1fa4788a7d3aef6982150919b7bd
YEBay1 <104377308+YEBay1@users.noreply.github.com>
2022-06-05 19:58:39 +0300

    Create AstronomyPictureoftheDay.py

---
+++ AstronomyPictureoftheDay.py
@@ -0,0 +1,26 @@
+import json
+from requests import Request, Session
+import cv2
+from translate import Translator
+import urllib.request
+
+url='https://api.nasa.gov/planetary/apod?api_key=WfGFbapMzkTX7cLCPG06ADSgBWtcumTTpOJHHOdt'
+translator= Translator(to_lang="tr")
+
+session = Session()
+response = session.get(url)
+veri=json.loads(response.content)
+
+baslik=veri['title']
+tarih=veri['date']
+aciklama=veri['explanation']
+resim=veri['hdurl']
+
+imgurl=resim
+urllib.request.urlretrieve(imgurl,"Astronomy.jpg")      # resmi indiririz
+
+print(baslik,tarih,aciklama,resim,sep="\n")
+
+img = cv2.imread ('Astronomy.jpg',1)                    # indirdiğimiz resmi gösteririz
+cv2.imshow ('image',img)
+cv2.waitKey(0)
