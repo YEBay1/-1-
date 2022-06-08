c110c32a29f66a2a002552eefbe8bdd92727eb45
YEBay1 <104377308+YEBay1@users.noreply.github.com>
2022-06-08 23:31:00 +0300

    Update Client.py

--- Socket/Client.py
+++ Socket/Client.py
@@ -1,62 +1,32 @@
-"""
-Client tarafını yazarken ek olarak recv() fonksiyonu ile mesajın tampon boyutunu (buffer size) ayarlamamız gerekiyor.
-Böylece bir sn de alınacak maksimum dosya boyutunu byte cinsinden ayarlamış oluyoruz.
-Bu boyutu ayarlarken değerimizin 2 nin üssü şeklinde olmasına dikkat ediniz (Örneğin 1024, 2048, 4096…)
-"""
-import socket
+import socket 
 
-# Socket oluşturulması
-s = socket.socket()
-
-# Bağlanılacak adres ve port
-host = "localhost"
-port = 15345            # Aynı port'a bağlan.Server'dan gelen mesajlar görülmeyebilir
+host="localhost"
+port = 12345 
 
 try:
-    # Bağlantıyı yap
-    s.connect((host, port))
-
-    # serverden yanıtı al
-    yanit = s.recv(1024)
-    print(yanit.decode("utf-8"))
-
-    # bağlantıyı kapat
-    s.close()
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print("Socket Oluşturuldu")
+    
+    s.bind((host,port))
+    print("Socket {} nolu porta bağlandı".format(port))
+    
+    s.listen(5) 
+    print("Socket dinleniyor")
+   
 except socket.error as msg:
-    print("[Server aktif değil.] Mesaj:", msg)
+    print("Hata",msg)
+    
+while True:
+    c,addr=s.accept()
+    print('Gelen bağlantı',addr)
+    
+    mesaj="Merhaba"
+    c.send(mesaj.encode('utf-8'))
+    
+    c.close()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
+ https://kerteriz.net/python-socket-programlama-nedir/
+"""
