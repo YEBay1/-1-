1622f05a93a28ad9a88302e63c10f46426a04581
YEBay <104377308+YEBay1@users.noreply.github.com>
2022-06-23 00:02:17 +0300

    Update WeeklyYield.py

--- WeeklyYield.py
+++ WeeklyYield.py
@@ -1,37 +1,81 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import openpyxl
+from openpyxl import Workbook
 
 def HaftalıkVerimHesaplama():
-    for i in range(1):
-        try:
-            a=float(input("Pazartesi -> "))
-            b=float(input("Salı -> "))
-            c=float(input("Çarşamba -> "))
-            d=float(input("Perşembe -> "))
-            e=float(input("Cuma -> "))
-            f=float(input("Cumartesi -> "))
-            g=float(input("Pazar -> "))
+    print("Günlerin önüne kaç saat çalıştığınızı yazın")
 
-        except ValueError:
-            print("Lütfen Bir Sayı Giriniz")
+    try:
+        a=float(input("Pazartesi -> "))
+        b=float(input("Salı -> "))
+        c=float(input("Çarşamba -> "))
+        d=float(input("Perşembe -> "))
+        e=float(input("Cuma -> "))
+        f=float(input("Cumartesi -> "))
+        g=float(input("Pazar -> "))
+
+    except ValueError:
+        print("Lütfen Bir Sayı Giriniz")
+
+    gunlukort = float(a + b + c + d + e + f + g) / 7
+    hours=[a,b,c,d,e,f,g]
+    gunler=["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"]
+    dosya="rapor.xlsx"
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+
+    for i in range(7):
+        c1=sheet.cell(row=i+1,column=1)
+        c1.value=gunler[i]
+
+        c2=sheet.cell(row=i+1,column=2)
+        c2.value = hours[i]
+
+        c3=sheet.cell(row=i+1,column=3)
+        c3.value=hours[i]*60
+
+    c4=sheet.cell(row=7,column=1)
+    c4.value="Haftalık rapor"
+
+    c5=sheet.cell(row=7,column=2)
+    c5.value=gunlukort
+
+    wb.save("rapor.xlsx")
 
 
-    günlükort=float(a+b+c+d+e+f+g)/7
-
-    if günlükort>=24:
+    if gunlukort>=24:
         print("HATALI VERİ GİRMİŞ OLABİLİRSİNİZ.LÜTFEN GİRDİĞİNİZ VERİLERİ KONTROL EDİN VEYA UYGULAMAYI TEKRAR ÇALIŞTIRIN")
     else:
-        saat = int(günlükort)
-        dakika = günlükort - int(günlükort)
+        saat = int(gunlukort)
+        dakika = gunlukort - int(gunlukort)
         dk = (saat * 60) + dakika
         print("Bu hafta Günlük Ortalama ", int(dk), " dakika çalışıldı","\n")
 
 
-    Q=input("Haftalık Verim Grafiğini Görmek için Q'ya tıklayın ve Girin --->")
-    if Q=="Q":
+    giris=input("Haftalık Verim Grafiğini Görmek için Q'ya,Haftalık Raporu Görmek için ise E'ye tıklayın --->")
+    if giris=="Q":
         haftx = np.array(["PTESİ","SALI","ÇŞAMBA","PŞEMBE","CUMA","CTESİ","PAZAR"])
         saaty = np.array([a, b, c, d, e, f, g])
         plt.plot(haftx, saaty)
         plt.show()
+    elif giris=="E":
+        f = open("rapor.xlsx", "r")     # Burada kullanıcı için dosyanın açılması gerekiyor
+        print(f.read())
     else:
         pass
+
+HaftalıkVerimHesaplama()
+
+
+"""
+Bu uygulama geliştirilecektir
+"""
+
+
+
+
+
+
+
+
